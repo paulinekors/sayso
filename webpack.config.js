9bf68f3af63ebd2907c2ae7213ec9efc3d8eb8f7
paulinekors <paulinekors@gmail.com>
2020-04-07 07:55:02 +0200

    implemented Rogier's feedback

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,38 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-const config = {
-  entry: [
-    './index.js',
-  ],
+module.exports = {
+  mode: 'development',
   module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loaders: [
-          'babel-loader',
-        ],
-        exclude: /node_modules/,
+
+  rules: [
+    {
+      test: /\.m?js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+        },
       },
-    ],
+    },
+  ],
+  },
+  entry: {
+    app: './src/index.js',
+  },
+  plugins: [
+    // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Development',
+      template: "index.html"
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
 };
-
-module.exports = config;
+ 
