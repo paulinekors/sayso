d582046fee312d8ca2b2d2af2888140f16e3aa73
paulinekors <paulinekors@gmail.com>
2020-05-06 08:19:34 +0200

    detailview.js

--- src/components/DetailView.js
+++ src/components/DetailView.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-function DetailView() {
-    return (
-        <div>Detail</div>
-    );
+function DetailView(props) {
+  const { error, status, message } = props;
+  const isLoading = status === 'pending';
+  const isResolved = status === 'resolved';
+  const isRejected = status === 'rejected';
+
+  return <div>Detail</div>;
 }
 
 export default DetailView;
