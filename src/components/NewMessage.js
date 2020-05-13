f73acb5e50cbf0a642f022f29ea195cb29351494
paulinekors <paulinekors@gmail.com>
2020-05-13 21:48:43 +0200

    set up for form new message

--- src/components/NewMessage.js
+++ src/components/NewMessage.js
@@ -1,11 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import AddIcon from '@material-ui/icons/Add';
+import Fab from '@material-ui/core/Fab';
+import Zoom from '@material-ui/core/Zoom';
 
 function NewMessage() {
+  const [isExpanded, setExpanded] = useState(false);
+
+  function expand() {
+    setExpanded(true);
+  }
 
   return (
     <div>
-      <h1>Required fields</h1>
-      <p>body title email firstName lastName title</p>
+      {/* <div>
+        <h1>Required fields</h1>
+        <p>title body email firstName lastName</p>
+      </div> */}
+      <form className="add-message">
+        {isExpanded && <input name="title" placeholder="Add a new message" />}
+        <textarea
+          name="content"
+          onClick={expand}
+          placeholder="Title:"
+          rows={isExpanded ? 2 : 0}
+        />
+        <textarea
+          name="name"
+          onClick={expand}
+          placeholder="Message:"
+          rows={isExpanded ? 5 : 0}
+        />
+        <textarea
+          name="content"
+          onClick={expand}
+          placeholder="First name:"
+          rows={isExpanded ? 1 : 0}
+        />
+        <textarea
+          name="content"
+          onClick={expand}
+          placeholder="Last name:"
+          rows={isExpanded ? 1 : 0}
+        />
+        <textarea
+          name="content"
+          onClick={expand}
+          placeholder="Email:"
+          rows={isExpanded ? 1 : 0}
+        />
+        <Zoom in={isExpanded}>
+          <Fab>
+            <AddIcon />
+          </Fab>
+        </Zoom>
+      </form>
     </div>
   );
 }
